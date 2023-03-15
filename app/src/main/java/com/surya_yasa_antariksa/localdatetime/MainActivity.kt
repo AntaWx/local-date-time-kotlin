6554611f6f21c2dff6e4784b7c098a7f7636b1a1
Surya-Yasa-Antariksa <suryagus80@gmail.com>
2023-03-15 09:39:27 +0800

    menambahkan fitur baru
     drawer layout belum berfungsi seperti ditutupi layout lain

--- app/src/main/java/com/surya_yasa_antariksa/localdatetime/MainActivity.kt
+++ app/src/main/java/com/surya_yasa_antariksa/localdatetime/MainActivity.kt
@@ -4,67 +4,135 @@ package com.surya_yasa_antariksa.localdatetime
     import android.content.Intent
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
+    import android.view.View
+    import android.widget.ImageButton
+    import android.widget.RelativeLayout
+    import android.widget.Toast
     import androidx.appcompat.app.AlertDialog
+    import androidx.appcompat.widget.SearchView
+    import androidx.appcompat.widget.SwitchCompat
+    import androidx.core.view.GravityCompat
+    import androidx.drawerlayout.widget.DrawerLayout
     import androidx.recyclerview.widget.LinearLayoutManager
     import androidx.recyclerview.widget.RecyclerView
     import androidx.recyclerview.widget.RecyclerView.VERTICAL
     import com.google.android.material.floatingactionbutton.FloatingActionButton
+    import com.google.android.material.switchmaterial.SwitchMaterial
     import com.surya_yasa_antariksa.localdatetime.database.DateTimeDatabase
     import com.surya_yasa_antariksa.localdatetime.database.entity.DateTimeEntity
 
-    class MainActivity : AppCompatActivity(), UserAdapter.OnItemClickListener {
+class MainActivity : AppCompatActivity(), UserAdapter.OnItemClickListener {
 
-        private lateinit var recyclerView: RecyclerView
-        private lateinit var addButton: FloatingActionButton
-        private var list = mutableListOf<DateTimeEntity>()
-        private lateinit var adapter: UserAdapter
-        private lateinit var database: DateTimeDatabase
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var addButton: FloatingActionButton
+    private var list = mutableListOf<DateTimeEntity>()
+    private lateinit var adapter: UserAdapter
+    private lateinit var database: DateTimeDatabase
+    private lateinit var findData: SearchView
+    private lateinit var parentSearchView: RelativeLayout
+    private lateinit var burgerBarIcon: ImageButton
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var switchMaterial: SwitchMaterial
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
 
-            recyclerView = findViewById(R.id.recycle_view)
-            addButton = findViewById(R.id.add_button)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            database = DateTimeDatabase.getInstance(applicationContext)
-            adapter = UserAdapter(list, this)
+        val actionBar = supportActionBar
 
-            recyclerView.adapter = adapter
-            recyclerView.layoutManager = LinearLayoutManager(applicationContext, VERTICAL, false)
+        actionBar?.hide()
 
-            addButton.setOnClickListener{
-                startActivity(Intent(this, MasukkanDataActivity::class.java))
+        recyclerView = findViewById(R.id.recycle_view)
+        addButton = findViewById(R.id.add_button)
+        findData = findViewById(R.id.cari_data)
+        parentSearchView = findViewById(R.id.cari_data_parent)
+        burgerBarIcon = findViewById(R.id.burger_bar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        switchMaterial = findViewById(R.id.switch_material)
+
+        database = DateTimeDatabase.getInstance(applicationContext)
+        adapter = UserAdapter(list, this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext, VERTICAL, false)
+
+        addButton.setOnClickListener{
+            startActivity(Intent(this, MasukkanDataActivity::class.java))
+        }
+
+        switchMaterial.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                // Switch button diaktifkan
+                Toast.makeText(this, "Switch button diaktifkan", Toast.LENGTH_SHORT).show()
+            } else {
+                // Switch button dinonaktifkan
+                Toast.makeText(this, "Switch button dinonaktifkan", Toast.LENGTH_SHORT).show()
             }
         }
 
-        override fun onResume() {
-            super.onResume()
-            getData()
-        }
-
-        @SuppressLint("NotifyDataSetChanged")
-        fun getData(){
-            list.clear()
-            list.addAll(database.getDateTimeDao().getAll())
-            adapter.notifyDataSetChanged()
-        }
-
-        override fun onUpdateClick(position: Int) {
-            // Kode yang akan dieksekusi saat tombol update di klik
-        }
-
-        override fun onDeleteClick(position: Int) {
-            val user = list[position]
-            AlertDialog.Builder(this)
-                .setTitle("Hapus Catatan")
-                .setMessage("Apakah Anda yakin ingin menghapus catatan ini?")
-                .setPositiveButton("Ya") { _, _ ->
-                    database.getDateTimeDao().delete(user)
-                    getData()
+        findData.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null && !query.isEmpty()) {
+                    searchByNama(query)
+                    findData.clearFocus()
+                    return true
                 }
-                .setNegativeButton("Tidak", null)
-                .create()
-                .show()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val nama = newText!!.trim()
+                searchByNama(nama)
+                return true
+            }
+        })
+
+        burgerBarIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        getData()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun getData(){
+        list.clear()
+        list.addAll(database.getDateTimeDao().getAll())
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onUpdateClick(position: Int) {
+    // Kode yang akan dieksekusi saat tombol update di klik
+    }
+
+    override fun onDeleteClick(position: Int) {
+        val user = list[position]
+        AlertDialog.Builder(this)
+            .setTitle("Hapus Catatan")
+            .setMessage("Apakah Anda yakin ingin menghapus catatan ini?")
+            .setPositiveButton("Ya") { _, _ ->
+                database.getDateTimeDao().delete(user)
+                getData()
+            }
+            .setNegativeButton("Tidak", null)
+            .create()
+            .show()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun searchByNama(nama: String){
+
+        val findNama = database.getDateTimeDao().getAll().filter { it.name!!.contains(nama, true) }
+
+        list.clear()
+        list.addAll(findNama)
+        if(findNama.isEmpty()){
+            Toast.makeText(this, "Nama yang dicari tidak ditemukan", Toast.LENGTH_SHORT).show()
+        }
+        adapter.notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/surya_yasa_antariksa/localdatetime/MasukkanDataActivity.kt
+++ app/src/main/java/com/surya_yasa_antariksa/localdatetime/MasukkanDataActivity.kt
@@ -1,13 +1,15 @@
 package com.surya_yasa_antariksa.localdatetime
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.EditText
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.widget.*
 import com.surya_yasa_antariksa.localdatetime.database.DateTimeDatabase
 import com.surya_yasa_antariksa.localdatetime.database.entity.DateTimeEntity
+import com.surya_yasa_antariksa.localdatetime.databinding.ActivityMasukkanDataBinding
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -18,16 +20,24 @@ class MasukkanDataActivity : AppCompatActivity() {
     private lateinit var datePicker: Button
     private lateinit var database: DateTimeDatabase
     private lateinit var saveButton: Button
+    private lateinit var binding: ActivityMasukkanDataBinding
+    private lateinit var arrowBack: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_masukkan_data)
-
+        binding = ActivityMasukkanDataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         masukanNama = findViewById(R.id.masukkan_nama)
         masukkanKegiatan = findViewById(R.id.kegiatan_column)
         datePicker = findViewById(R.id.date_picker)
         saveButton = findViewById(R.id.simpan)
         database = DateTimeDatabase.getInstance(applicationContext)
+        arrowBack = findViewById(R.id.arrow_back)
+
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val actionBar = supportActionBar
+
+        actionBar?.hide()
 
         // Menambahkan OnClickListener untuk datePicker
         datePicker.setOnClickListener {
@@ -37,25 +47,52 @@ class MasukkanDataActivity : AppCompatActivity() {
             val datePickerDialog = DatePickerDialog(this, { _, dayOfMonth, month, year ->
                 // Membuat objek LocalDateTime dari tanggal yang dipilih
                 val selectedDate = LocalDateTime.of(dayOfMonth, month+1, year, 0, 0)
-                val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
                 // Mengubah LocalDateTime menjadi String menggunakan format tertentu
                 val formattedDate = selectedDate.format(formatter)
                 // Menampilkan tanggal yang dipilih di EditText
                 datePicker.text = formattedDate
+                // Menampilkan tanggal yang dipilih di EditText dan menghapus tanda error
+                datePicker.apply {
+                    setText(formattedDate)
+                    error = null
+                }
             }, currentDate.year, currentDate.monthValue-1, currentDate.dayOfMonth)
             // Menampilkan DatePickerDialog
             datePickerDialog.show()
+        }
 
-            saveButton.setOnClickListener {
-                val nama = masukanNama.text.toString()
-                val kegiatan = masukkanKegiatan.text.toString()
-                val tanggal = datePicker.text.toString()
+        saveButton.setOnClickListener {
+            val nama = masukanNama.text.toString()
+            val kegiatan = masukkanKegiatan.text.toString()
+            val tanggal = datePicker.text.toString()
 
-                val dataEntity = DateTimeEntity(0, nama, tanggal, kegiatan)
-
-                database.getDateTimeDao().addAll(dataEntity)
-                finish()
+            when{
+                nama.isEmpty() ->{
+                    binding.masukkanNama.error = "Nama Harus Diisi"
+                    binding.masukkanNama.requestFocus()
+                    return@setOnClickListener
+                }
+                kegiatan.isEmpty() ->{
+                    binding.kegiatanColumn.error = "Kegiatan harus diisi"
+                    binding.kegiatanColumn.requestFocus()
+                    return@setOnClickListener
+                }
+                tanggal.isEmpty() ->{
+                    binding.datePicker.error = "tanggal harus diisi"
+                    binding.datePicker.requestFocus()
+                    return@setOnClickListener
+                }
             }
+            val dataEntity = DateTimeEntity(0, nama, tanggal, kegiatan)
+
+            database.getDateTimeDao().addAll(dataEntity)
+            Toast.makeText(this, "data berhasil disimpan", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        arrowBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
